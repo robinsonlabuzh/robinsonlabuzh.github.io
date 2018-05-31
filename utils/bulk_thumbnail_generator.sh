281f5c0ebcd1cbc777be04eef93e77a25ceafec9
Izaskun Mallona <izaskun.mallona@gmail.com>
2018-05-31 13:47:37 +0200

    Lightweight images for people.html #22 (#24)
    * Profile added (Izaskun)
    
    * Replacing people's photos by thumbnails (placed at the /img/thumbnail folder) issue #22
    
    Note that images are both resized and compressed; downsizing appears to be ok for most of them but some turn out to be blurry due to their small starting size/resolution.
    
    Created an utils folder with a bash script to call imagemagick to generate the thumbnails
    
    * Updating Stephan image and thumbnail; and removing hardtyped constant #22
    
    * Added new Simone's photo (#22)
    
    * Education formatting as everyone (thanks @lmweber #15), linking Simone's new photograph

---
+++ utils/bulk_thumbnail_generator.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#
+# Generates thumbnails of any image while maintaining the image filetype
+#
+# 30rd May 2018 Izaskun Mallona
+# Inspired by https://www.smashingmagazine.com/2015/06/efficient-image-resizing-with-imagemagick/
+
+
+THUMBNAIL_WIDTH=300
+INPUT_PATH=.
+OUTPUT_PATH=thumbnail
+mkdir -p "$OUTPUT_PATH"
+
+# # To mogrify the whole path
+# mogrify -path "$OUTPUT_PATH" -filter Triangle -define filter:support=2 -thumbnail "$THUMBNAIL_WIDTH" \
+#         -dither None -posterize 136 -quality 82 \
+#         -define png:compression-filter=5 -define jpeg:fancy-upsampling=off  \
+#         -define png:compression-level=9 -define png:compression-strategy=1 \
+#         -define png:exclude-chunk=all -interlace none -colorspace sRGB \
+#         "$INPUT_PATH"
+
+# Does not mogrify the whole directory buth rather processes the files over a given threshold,
+#  copying the original version of those below the selected size limit
+# Looks for images matching "*jpg" or "*png" filenames
+
+SIZE_THRES=100
+
+mkdir -p thumbnail
+
+for fn in $(find "$INPUT_PATH" -name "*jpg" -or -name "*png")
+do
+    echo $fn
+    filesize=$(ls -l $fn | awk '{print $5}')       
+    if [[ "$filesize" -gt "$SIZE_THRES" ]] 
+    then 
+        name=$(basename "$fn" | cut -f 1 -d '.')
+
+        convert "$fn" -filter Triangle -define filter:support=2 -thumbnail "$THUMBNAIL_WIDTH" \
+                -dither None -posterize 136 -quality 82 \
+                -define png:compression-filter=5 -define jpeg:fancy-upsampling=off  \
+                -define png:compression-level=9 -define png:compression-strategy=1 \
+                -define png:exclude-chunk=all -interlace none -colorspace sRGB \
+                "$OUTPUT_PATH"/"$(basename $fn)"
+
+        
+    else
+        rsync -avt $fn thumbnail/"$(basename $fn)"
+    fi
+done
+
+
